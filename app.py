9bbf3246090ff143583a1fba7953d14ec9a8b0ed
qa-kks <afreeca.probe@gmail.com>
2026-06-23 11:39:30 +0900

    Add Jenkis pytest practice

---
+++ app.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    return a + b
+
+
+def is_even(number):
+    return number % 2 == 0
---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from app import add, is_even
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_is_even():
+    assert is_even(4) is True
+
+
+def test_is_not_even():
+    assert is_even(5) is False
